a80b2b38a4339524b72e88ea70543c8c41e53b30
jv <jv@BR2L11873L.nimbus.local>
2022-03-29 09:59:47 -0300

    Basic Auth Hard Coded

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from flask import Flask
+from flask_httpauth import HTTPBasicAuth
+
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+@auth.verify_password
+def authenticate (username, password):
+    if username and password:
+        if username == 'jv' and password == '123':
+            return True
+    return False
 
 from controller import *
 
 @app.route('/')
+@auth.login_required
 def welcome():
     return "My book API"
--- controller/user_controller.py
+++ controller/user_controller.py
@@ -1,21 +1,32 @@
-from app import app
+from app import app, auth
 from model.user_model import user_model
 from flask import request
 
 obj = user_model()
 
+# GETS
 @app.route('/books/getall')
+@auth.login_required
 def books_getall_controller():
     return obj.books_getall_model()
 
+
+# POSTS
 @app.route('/books/add', methods=['POST'])
+@auth.login_required
 def books_add_controller():
     return obj.books_add_model(request.form)
 
+
+# PUTS
 @app.route('/books/update', methods=['PUT'])
+@auth.login_required
 def books_update_controller():
     return obj.books_update_model(request.form)
 
+
+# DELETES
 @app.route('/books/delete/<id>', methods=['DELETE'])
+@auth.login_required
 def books_delete_controller(id):
     return obj.books_delete_model(id)
