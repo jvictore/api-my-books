6139f1a6baa972ad799ac2a6439bd1e2e18e9550
Joao Costa <joao.costa@la.logicalis.com>
2022-05-03 16:46:00 -0300

    Integracao flask/docker com auth

--- app.py
+++ app.py
@@ -3,18 +3,18 @@ from flask_httpauth import HTTPBasicAuth
 
 
 app = Flask(__name__)
-# auth = HTTPBasicAuth()
+auth = HTTPBasicAuth()
 
-# @auth.verify_password
-# def authenticate (username, password):
-#     if username and password:
-#         if username == 'jv' and password == '123':
-#             return True
-#     return False
+@auth.verify_password
+def authenticate (username, password):
+    if username and password:
+        if username == 'jv' and password == '123':
+            return True
+    return False
 
 from controller import *
 
 @app.route('/')
-# @auth.login_required
+@auth.login_required
 def welcome():
     return "My book API"
