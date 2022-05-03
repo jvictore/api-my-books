0fc17477321c571a8fe893d568af333306f69855
Joao Costa <joao.costa@la.logicalis.com>
2022-05-03 16:34:58 -0300

    Integracao flask/docker inicial

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,24 @@
 FROM ubuntu:bionic
-RUN apt-get update
-RUN apt-get install python3 -y && apt-get install python3-pip -y
+
+RUN apt update -y && apt upgrade -y
+RUN apt install software-properties-common -y
+RUN add-apt-repository ppa:deadsnakes/ppa
+
+RUN apt install python3.9 -y && apt-get install python3-pip -y
 RUN apt-get update -y && apt-get upgrade -y
 RUN pip3 install --upgrade pip
+RUN apt-get update && apt-get upgrade
+
+WORKDIR /app
+COPY . /app
+
+RUN apt install libmysqlclient-dev python3-dev -y
+RUN pip3 --no-cache-dir install -r requirements.txt
+
+ENV FLASK_APP=app.py
+ENV LC_ALL=C.UTF-8
+ENV LANG=C.UTF-8
+
+EXPOSE 5000
+
+CMD ["flask", "run", "--host=0.0.0.0"]
--- controller/user_controller.py
+++ controller/user_controller.py
@@ -1,42 +1,46 @@
-from app import app, auth
+# from app import app, auth
+from app import app
 from model.user_model import user_model
 from flask import request
 
 obj = user_model()
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
+
 # GETS
 @app.route('/books/getall')
-@auth.login_required
+# @auth.login_required
 def books_get_all_controller():
     return obj.books_get_all_model()
 
 @app.route('/books/getone/<id>')
-@auth.login_required
+# @auth.login_required
 def books_get_one_controller(id):
     return obj.books_get_one_model(id)
 
 @app.route('/books/gettotalpages')
-@auth.login_required
+# @auth.login_required
 def books_get_total_pages_controller():
     return obj.books_get_total_pages_model()
 
 
 # POSTS
 @app.route('/books/add', methods=['POST'])
-@auth.login_required
+# @auth.login_required
 def books_add_controller():
     return obj.books_add_model(request.form)
 
 
 # PUTS
 @app.route('/books/update', methods=['PUT'])
-@auth.login_required
+# @auth.login_required
 def books_update_controller():
     return obj.books_update_model(request.form)
 
 
 # DELETES
 @app.route('/books/delete/<id>', methods=['DELETE'])
-@auth.login_required
+# @auth.login_required
 def books_delete_controller(id):
     return obj.books_delete_model(id)
